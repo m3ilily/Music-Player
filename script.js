0eb9d27b6e19dbb72b57391e0c0dec022996ef59
m3ilily <meilinsong667@gmail.com>
2024-08-07 02:07:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,254 @@
+$(function () {
+  var playerTrack = $("#player-track"),
+    bgArtwork = $("#bg-artwork"),
+    bgArtworkUrl,
+    albumName = $("#album-name"),
+    trackName = $("#track-name"),
+    albumArt = $("#album-art"),
+    sArea = $("#s-area"),
+    seekBar = $("#seek-bar"),
+    trackTime = $("#track-time"),
+    insTime = $("#ins-time"),
+    sHover = $("#s-hover"),
+    playPauseButton = $("#play-pause-button"),
+    i = playPauseButton.find("i"),
+    tProgress = $("#current-time"),
+    tTime = $("#track-length"),
+    seekT,
+    seekLoc,
+    seekBarPos,
+    cM,
+    ctMinutes,
+    ctSeconds,
+    curMinutes,
+    curSeconds,
+    durMinutes,
+    durSeconds,
+    playProgress,
+    bTime,
+    nTime = 0,
+    buffInterval = null,
+    tFlag = false,
+    albums = [
+      "緑の手紙",
+      "Kimi ga Kureta Mono",
+      "Deep Coma",
+      "The History",
+      "Fatal Love"
+    ],
+    trackNames = [
+      "ミツキヨ (Mitsukiyo)",
+      "Zone",
+      "春野 (Haruno)",
+      "Jannik",
+      "Jori King"
+    ],
+    albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
+    trackUrl = [
+      "https://m3ilily.github.io/Music-Files/Mitsukiyo%20-%20'The%20Green%20Letter'-%20The%20Green%20Letter.mp3",
+      
+ "https://m3ilily.github.io/Music-Files/%E6%81%AD%E4%B8%80%E9%83%8E%20%26%20LambSoarssecret%20base%20%E5%90%9B%E3%81%8B%E3%82%99%E3%81%8F%E3%82%8C%E3%81%9F%E3%82%82%E3%81%AE%20%20ZONE.mp3",
+      
+ "https://m3ilily.github.io/Music-Files/%E6%98%A5%E9%87%8E%20%E6%B7%B1%E6%98%8F%E7%9D%A1%20from%20Live%20Session%20'23%20(Acoustic).mp3",
+      
+ "https://m3ilily.github.io/Music-Files/%E4%B8%AD%E8%A5%BF%E5%90%88%E7%92%A7%E6%B5%AE%E5%85%89The%20History%E5%8F%A4%E7%AD%9D%E4%B8%8E%E5%A4%A7%E6%8F%90%E7%90%B4%E7%9A%84%E5%AE%BF%E5%91%BD%E6%84%9F%E5%9C%A8%E6%AD%A4%E5%88%BB%E6%9C%89%E4%BA%86%E7%94%BB%E9%9D%A2Musical%20Instruments%20Guzheng%20Cover%20%E7%A2%B0%E7%A2%B0%E5%BD%AD%E7%A2%B0%E5%BD%ADJingxuan.mp3",
+      
+ "https://m3ilily.github.io/Music-Files/Fatal%20Love%20(Explicit)%20-%20Jori%20King.mp3"
+    ],
+    playPreviousTrackButton = $("#play-previous"),
+    playNextTrackButton = $("#play-next"),
+    currIndex = -1;
+
+  function playPause() {
+    setTimeout(function () {
+      if (audio.paused) {
+        playerTrack.addClass("active");
+        albumArt.addClass("active");
+        checkBuffering();
+        i.attr("class", "fas fa-pause");
+        audio.play();
+      } else {
+        playerTrack.removeClass("active");
+        albumArt.removeClass("active");
+        clearInterval(buffInterval);
+        albumArt.removeClass("buffering");
+        i.attr("class", "fas fa-play");
+        audio.pause();
+      }
+    }, 300);
+  }
+
+  function showHover(event) {
+    seekBarPos = sArea.offset();
+    seekT = event.clientX - seekBarPos.left;
+    seekLoc = audio.duration * (seekT / sArea.outerWidth());
+
+    sHover.width(seekT);
+
+    cM = seekLoc / 60;
+
+    ctMinutes = Math.floor(cM);
+    ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
+
+    if (ctMinutes < 0 || ctSeconds < 0) return;
+
+    if (ctMinutes < 0 || ctSeconds < 0) return;
+
+    if (ctMinutes < 10) ctMinutes = "0" + ctMinutes;
+    if (ctSeconds < 10) ctSeconds = "0" + ctSeconds;
+
+    if (isNaN(ctMinutes) || isNaN(ctSeconds)) insTime.text("--:--");
+    else insTime.text(ctMinutes + ":" + ctSeconds);
+
+    insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
+  }
+
+  function hideHover() {
+    sHover.width(0);
+    insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
+  }
+
+  function playFromClickedPos() {
+    audio.currentTime = seekLoc;
+    seekBar.width(seekT);
+    hideHover();
+  }
+
+  function updateCurrTime() {
+    nTime = new Date();
+    nTime = nTime.getTime();
+
+    if (!tFlag) {
+      tFlag = true;
+      trackTime.addClass("active");
+    }
+
+    curMinutes = Math.floor(audio.currentTime / 60);
+    curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
+
+    durMinutes = Math.floor(audio.duration / 60);
+    durSeconds = Math.floor(audio.duration - durMinutes * 60);
+
+    playProgress = (audio.currentTime / audio.duration) * 100;
+
+    if (curMinutes < 10) curMinutes = "0" + curMinutes;
+    if (curSeconds < 10) curSeconds = "0" + curSeconds;
+
+    if (durMinutes < 10) durMinutes = "0" + durMinutes;
+    if (durSeconds < 10) durSeconds = "0" + durSeconds;
+
+    if (isNaN(curMinutes) || isNaN(curSeconds)) tProgress.text("00:00");
+    else tProgress.text(curMinutes + ":" + curSeconds);
+
+    if (isNaN(durMinutes) || isNaN(durSeconds)) tTime.text("00:00");
+    else tTime.text(durMinutes + ":" + durSeconds);
+
+    if (
+      isNaN(curMinutes) ||
+      isNaN(curSeconds) ||
+      isNaN(durMinutes) ||
+      isNaN(durSeconds)
+    )
+      trackTime.removeClass("active");
+    else trackTime.addClass("active");
+
+    seekBar.width(playProgress + "%");
+
+    if (playProgress == 100) {
+      i.attr("class", "fa fa-play");
+      seekBar.width(0);
+      tProgress.text("00:00");
+      albumArt.removeClass("buffering").removeClass("active");
+      clearInterval(buffInterval);
+    }
+  }
+
+  function checkBuffering() {
+    clearInterval(buffInterval);
+    buffInterval = setInterval(function () {
+      if (nTime == 0 || bTime - nTime > 1000) albumArt.addClass("buffering");
+      else albumArt.removeClass("buffering");
+
+      bTime = new Date();
+      bTime = bTime.getTime();
+    }, 100);
+  }
+
+  function selectTrack(flag) {
+    if (flag == 0 || flag == 1) ++currIndex;
+    else --currIndex;
+
+    if (currIndex > -1 && currIndex < albumArtworks.length) {
+      if (flag == 0) i.attr("class", "fa fa-play");
+      else {
+        albumArt.removeClass("buffering");
+        i.attr("class", "fa fa-pause");
+      }
+
+      seekBar.width(0);
+      trackTime.removeClass("active");
+      tProgress.text("00:00");
+      tTime.text("00:00");
+
+      currAlbum = albums[currIndex];
+      currTrackName = trackNames[currIndex];
+      currArtwork = albumArtworks[currIndex];
+
+      audio.src = trackUrl[currIndex];
+
+      nTime = 0;
+      bTime = new Date();
+      bTime = bTime.getTime();
+
+      if (flag != 0) {
+        audio.play();
+        playerTrack.addClass("active");
+        albumArt.addClass("active");
+
+        clearInterval(buffInterval);
+        checkBuffering();
+      }
+
+      albumName.text(currAlbum);
+      trackName.text(currTrackName);
+      albumArt.find("img.active").removeClass("active");
+      $("#" + currArtwork).addClass("active");
+
+      bgArtworkUrl = $("#" + currArtwork).attr("src");
+
+      bgArtwork.css({ "background-image": "url(" + bgArtworkUrl + ")" });
+    } else {
+      if (flag == 0 || flag == 1) --currIndex;
+      else ++currIndex;
+    }
+  }
+
+  function initPlayer() {
+    audio = new Audio();
+
+    selectTrack(0);
+
+    audio.loop = false;
+
+    playPauseButton.on("click", playPause);
+
+    sArea.mousemove(function (event) {
+      showHover(event);
+    });
+
+    sArea.mouseout(hideHover);
+
+    sArea.on("click", playFromClickedPos);
+
+    $(audio).on("timeupdate", updateCurrTime);
+
+    playPreviousTrackButton.on("click", function () {
+      selectTrack(-1);
+    });
+    playNextTrackButton.on("click", function () {
+      selectTrack(1);
+    });
+  }
+
+  initPlayer();
+});
